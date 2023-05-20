089bb5f99427eafaafeca1959841e7a74ba4fb5b
kirill <kirypleshkov@mail.ru>
2023-05-20 16:54:11 +0700

    v 1.12(processor)

--- src/processor/processor.controller.ts
+++ src/processor/processor.controller.ts
@@ -3,6 +3,7 @@ import {ProcessorCreateSeriesDto} from "./dto/processor-create-series.dto";
 import {ProcessorService} from "./processor.service";
 import {ProcessorCreateCodeNameDto} from "./dto/processor-create-code-name.dto";
 import {ProcessorCreateDto} from "./dto/processor-create.dto";
+import {ProcessorGetDto} from "./dto/processor-get.dto";
 
 @Controller('processor')
 export class ProcessorController {
@@ -22,6 +23,11 @@ export class ProcessorController {
     @Post('/create')
     create(@Body() processorCreateDto: ProcessorCreateDto) {
         return this.processorService.create(processorCreateDto)
+    }
+
+    @Post('/all-characteristics')
+    getAllCharacteristics(@Body() processorGetDto: ProcessorGetDto) {
+        return this.processorService.getAllCharacteristics(processorGetDto)
     }
 
 }
--- src/processor/processor.service.ts
+++ src/processor/processor.service.ts
@@ -6,6 +6,7 @@ import {ProcessorCodeNameModel} from "./models/processor-code-name.model";
 import {ProcessorCreateCodeNameDto} from "./dto/processor-create-code-name.dto";
 import {ProcessorCreateDto} from "./dto/processor-create.dto";
 import {ProcessorModel} from "./models/processor.model";
+import {ProcessorGetDto} from "./dto/processor-get.dto";
 
 @Injectable()
 export class ProcessorService {
@@ -23,6 +24,21 @@ export class ProcessorService {
 
     async create(processorCreateDto: ProcessorCreateDto) {
         return await this.processorRepository.create(processorCreateDto)
+    }
+
+    async getAllCharacteristics(processorGetDto: ProcessorGetDto) {
+        const models = await this.processorRepository.findAll({include: [ProcessorCodeNameModel, ProcessorSeriesModel]})
+
+        let result = []
+
+        models.map((element) => {
+
+            const name = element.series.name + ' ' + element.codeName.name
+
+            result.push({url: element.url, id: element.id, name: name})
+        })
+
+        return result
     }
 
 }
